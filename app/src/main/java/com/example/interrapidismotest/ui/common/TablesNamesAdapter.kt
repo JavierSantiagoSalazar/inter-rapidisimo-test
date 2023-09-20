dd65f87916662b3cbdd5aafa8b821ec9d0767b56
Javier Santiago Salazar <100961042+JavierSantiagoSalazar@users.noreply.github.com>
2023-09-19 22:44:56 -0500

    #2PR Whole app functionality with Activity and ViewModel. (#2)
    * MenuFragmen and ViewModel functionality (Filter and show), shows and hide the data, observers setted, some extension functions created.

---
+++ app/src/main/java/com/example/interrapidismotest/ui/common/TablesNamesAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.interrapidismotest.ui.common
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.interrapidismotest.R
+import com.example.interrapidismotest.databinding.ItemTableNameBinding
+import com.example.interrapidismotest.domain.Table
+
+class TablesNamesAdapter :
+    ListAdapter<Table, TablesNamesAdapter.ViewHolder>(basicDiffUtil { old, new ->
+        old.tableName == new.tableName
+    }) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = parent.inflate(R.layout.item_table_name, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val movie = getItem(position)
+        holder.bind(movie)
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = ItemTableNameBinding.bind(view)
+        fun bind(table: Table) = with(binding) {
+            tvTableName.text = table.tableName
+        }
+    }
+}
--- app/src/main/java/com/example/interrapidismotest/ui/tablesmenu/MenuFragment.kt
+++ app/src/main/java/com/example/interrapidismotest/ui/tablesmenu/MenuFragment.kt
@@ -2,6 +2,7 @@ package com.example.interrapidismotest.ui.tablesmenu
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.interrapidismotest.App
@@ -10,6 +11,14 @@ import com.example.interrapidismotest.data.repository.TableRepository
 import com.example.interrapidismotest.dataapp.database.TableLocalDataSourceImpl
 import com.example.interrapidismotest.dataapp.server.TableRemoteDataSourceImpl
 import com.example.interrapidismotest.databinding.FragmentMenuBinding
+import com.example.interrapidismotest.ui.common.TablesNamesAdapter
+import com.example.interrapidismotest.ui.common.diff
+import com.example.interrapidismotest.ui.common.launchAndCollect
+import com.example.interrapidismotest.ui.common.setVisibleOrGone
+import com.example.interrapidismotest.ui.common.showErrorSnackBar
+import com.example.interrapidismotest.ui.common.toggleText
+import com.example.interrapidismotest.ui.common.toggleVisibility
+import com.example.interrapidismotest.usecases.tables.GetTablesUseCase
 import com.example.interrapidismotest.usecases.tables.RequestTablesUseCase
 
 
@@ -22,18 +31,56 @@ class MenuFragment : Fragment(R.layout.fragment_menu) {
             TableRemoteDataSourceImpl("admin")
         )
         MenuViewModelFactory(
+            GetTablesUseCase(repository),
             RequestTablesUseCase(repository)
         )
     }
 
+    private val adapter = TablesNamesAdapter()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val binding = FragmentMenuBinding.bind(view)
+        val binding = FragmentMenuBinding.bind(view).apply {
+            recyclerTables.adapter = adapter
+        }
 
-        binding.getDataButton.setOnClickListener {
-            viewModel.onUiReady()
+        with(binding) {
+            getTablesDataButton.setOnClickListener { viewModel.getTables() }
+            showDataButton.setOnClickListener {
+                binding.lyTablesScreen.toggleVisibility()
+                showDataButton.toggleText(
+                    getString(R.string.menu_button_show_data),
+                    getString(R.string.menu_button_hide_data)
+                )
+                tvFull.setVisibleOrGone(!tvEmpty.isShown && !etFilter.isShown)
+            }
+        }
+
+        with(viewModel.state) {
+            diff(this, { it.loading }) {
+                it.let { binding.progress.setVisibleOrGone(it) }
+            }
+            diff(this, { it.tables }) { tables ->
+                binding.etFilter.addTextChangedListener { userFilter ->
+                    val filteredTablesList = tables?.filter { table ->
+                        table.tableName.lowercase().contains(userFilter.toString().lowercase())
+                    }
+                    adapter.submitList(filteredTablesList)
+                }
+                adapter.submitList(tables)
+                binding.tvFull.setVisibleOrGone(tables?.isNotEmpty() ?: false && !binding.recyclerTables.isShown)
+                binding.tvEmpty.setVisibleOrGone(tables?.isEmpty() ?: true)
+            }
+            launchAndCollect(this) {
+                it.error?.let { error ->
+                    if (view.isShown) {
+                        view.showErrorSnackBar(error) {
+                            viewModel.getTables()
+                        }
+                    }
+                }
+            }
         }
     }
-
 }
